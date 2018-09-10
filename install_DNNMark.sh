3b03cdb8ed99b2d4fe53676d8e7bd0188bf8ad4a
Peter Bryzgalov <peterbryz@yahoo.com>
2018-09-10 22:16:44 +0900

    Installation scripts

---
+++ install_DNNMark.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt-get install -y cmake libgflags-dev libgoogle-glog-dev
+git clone https://github.com/pyotr777/DNNMark.git
+mkdir -p ~/cudnn
+cd DNNMark
+git checkout convolutions
+./setup.sh CUDA
+cd build
+make
+
+
