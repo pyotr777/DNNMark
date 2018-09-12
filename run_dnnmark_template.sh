70f36c94efb7cd130450a17aa414f98cbe199e62
Peter Bryzgalov <peterbryz@yahoo.com>
2018-09-12 07:03:15 +0000

    Removed debug info from run_dnnmark_template.sh

--- run_dnnmark_template.sh
+++ run_dnnmark_template.sh
@@ -24,8 +24,6 @@ Configuration saved in temporary file conf_tmp.dnnmark
 USAGEBLOCK
 )
 
-#echo "Pars: $@"
-#echo "env:$(env)"
 
 template="conf_multiconv_mod.dnntemplate"
 config_file="conf_tmp.dnnmark"
@@ -77,7 +75,6 @@ while test $# -gt 0; do
             CBFA="$2";shift;
             ;;
         --iter)
-            echo "Using iterations $2";
             ITER="$2";shift;
             ;;
         --debug)
