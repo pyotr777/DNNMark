6a9b54fa8b1ff6dbf266558209edcb4e370cd85a
Peter Bryzgalov <peterbryz@yahoo.com>
2018-12-11 15:02:29 +0900

    Measure filler time for convolution layers

--- core/include/layers/conv_layer.h
+++ core/include/layers/conv_layer.h
@@ -25,7 +25,7 @@
 
 #include "dnn_layer.h"
 #include <iostream>
-
+#include "timer.h"
 namespace dnnmark {
 
 template <typename T>
@@ -264,10 +264,16 @@ class ConvolutionLayer : public Layer<T> {
     // Fill the bottom data
     if (p_dnnmark_->getRunMode() == STANDALONE ||
         !previous_layer_name_.compare("null")) {
+      // Timer
+      StopWatch timer_c;
+      timer_c.Start();
       for (int i = 0; i < num_bottoms_; i++) {
         bottoms_[i]->Filler();
       }
+      timer_c.Stop();
+      LOG(INFO) << "FWD filler time(ms): "<<  timer_c.DiffInMs();
     }
+
     // Convolution forward computation
     for (int i = 0; i < num_bottoms_; i++) {
       dnnmarkConvolutionForward(
@@ -288,6 +294,9 @@ class ConvolutionLayer : public Layer<T> {
   void BackwardPropagation() {
     if (p_dnnmark_->getRunMode() == STANDALONE ||
         !previous_layer_name_.compare("null")) {
+      // Timer
+      StopWatch timer_c;
+      timer_c.Start();
       // Fill the top data and top diff data
       for (int i = 0; i < num_tops_; i++) {
         tops_[i]->Filler();
@@ -297,6 +306,8 @@ class ConvolutionLayer : public Layer<T> {
       for (int i = 0; i < num_bottoms_; i++) {
         bottoms_[i]->Filler();
       }
+      timer_c.Stop();
+      LOG(INFO) << "BWD filler time(ms): "<<  timer_c.DiffInMs();
     }
 
     // Convolution forward computation
